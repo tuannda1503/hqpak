be5f9ee047022b7b3810680ef44e675e9b395794
tuannda1503 <ndatuancntt@gmail.com>
2022-11-24 17:45:43 +0700

    create migration orders

---
+++ database/migrations/2022_11_24_093123_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customerName')->require();
+            $table->string('phone')->nullable();
+            $table->timestamp('orderAt')->nullable();
+            $table->timestamp('deliveryDate')->nullable();
+            $table->string('productName');
+            $table->boolean('getItNow')->default(false);
+            $table->integer('amount');
+            $table->string('email')->nullable();
+            $table->string('zalo')->nullable();
+            $table->string('quoteWith')->default(0); //0: email, 1: zalo
+            $table->integer('width');
+            $table->integer('height');
+            $table->string('material');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
